2da1c15b557f6cc593bd7c31fee5d07ecd9752c6
xistne <du20063@gmail.com>
2023-11-29 23:01:46 +0900

    7장 오류 있음..

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -19,6 +19,11 @@ public class ArticleController {
     @Autowired
     private ArticleRepository articleRepository;
 
+    @GetMapping("/articles/new")
+    public String newArticleForm() {
+        return "articles/new";
+    }
+
     @PostMapping("/articles/create")
     public String createArticle(ArticleForm form) {
         //System.out.println(form.toString());
@@ -31,6 +36,7 @@ public class ArticleController {
         log.info(saved.toString());
         return "redirect:/articles/" + saved.getId();
     }
+
     @GetMapping("/articles/{id}")
     public String show(@PathVariable Long id, Model model) {
         log.info("id = " + id);
@@ -38,10 +44,30 @@ public class ArticleController {
         model.addAttribute("article", articleEntity);
         return "articles/show";
     }
+
     @GetMapping("/articles")
-    public String index(Model model){
+    public String index(Model model) {
         ArrayList<Article> articleEntityList = articleRepository.findAll();
         model.addAttribute("articleList", articleEntityList);
         return "articles/index";
+    }
+
+    @GetMapping("/articles/{id}/edit")
+    public String edit(@PathVariable Long id, Model model) {
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+        model.addAttribute("article", articleEntity);
+        return "articles/edit";
+    }
+
+    @PostMapping("/articles/update")
+    public String update(ArticleForm form) {
+        log.info(form.toString());
+        Article articleEntity = form.toEntity();
+        log.info(articleEntity.toString());
+        Article target = articleRepository.findById(articleEntity.getId()).orElse(null);
+        if(target != null) {
+            articleRepository.save(articleEntity);
+        }
+        return "redirect:/articles/" + articleEntity.getId();
     }
 }
