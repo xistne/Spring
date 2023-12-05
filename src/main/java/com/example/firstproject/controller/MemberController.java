e368d7ec8062ff047d138a8c5dfc924d5761e45f
xistne <du20063@gmail.com>
2023-12-05 22:11:39 +0900

    Chapter 8

--- src/main/java/com/example/firstproject/controller/MemberController.java
+++ src/main/java/com/example/firstproject/controller/MemberController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.ArrayList;
 
@@ -65,5 +66,14 @@ public class MemberController {
             memberRepository.save(memberEntity);
         }
         return "redirect:/members/" + memberEntity.getId();
+    }
+    @GetMapping("/members/{id}/delete")
+    public String delete(@PathVariable Long id, RedirectAttributes rttr) {
+        Member target = memberRepository.findById(id).orElse(null);
+        if(target != null) {
+            memberRepository.delete(target);
+            rttr.addFlashAttribute("msg","삭제되었습니다.");
+        }
+        return "redirect:/members";
     }
 }
