2fcbe33fe6bc022bf2f59da3a73655be855dd8ca
xistne <du20063@gmail.com>
2023-12-05 22:42:06 +0900

    Chapter 9

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
-INSERT INTO article(id, title, content) VALUES(1, '가가가가','1111');
-INSERT INTO article(id, title, content) VALUES(2, '나나나나','2222');
-INSERT INTO article(id, title, content) VALUES(3, '다다다다','3333');
+INSERT INTO article(title, content) VALUES('가가가가','1111');
+INSERT INTO article(title, content) VALUES('나나나나','2222');
+INSERT INTO article(title, content) VALUES('다다다다','3333');
 
-INSERT INTO member(id, email, password) VALUES(1, 'aaa','111');
-INSERT INTO member(id, email, password) VALUES(2, 'bbb','222');
-INSERT INTO member(id, email, password) VALUES(3, 'ccc','333');
+INSERT INTO member(email, password) VALUES('aaa','111');
+INSERT INTO member(email, password) VALUES('bbb','222');
+INSERT INTO member(email, password) VALUES('ccc','333');
